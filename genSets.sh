1a5dc0ac462e022282a9192c5d0180d0466402f0
Martin Smith <msmith@ucsd.edu>
2014-12-04 21:30:34 -0800

    Added qsub option to runSets.sh

--- genSets.sh
+++ genSets.sh
@@ -2,7 +2,4 @@
 for k in *.csv; do
   newname=$k.sim;
   python3 ../simsetgen.py 4 $k $newname ;
-  #echo $newname;
-  #echo "Hello!";
-#do echo "((echo 1; echo `pwd`/$k; echo `pwd`/tree.nwk; echo 1; echo d) | /usr/local/bin/HYPHYMP /usr/local/lib/hyphy/TemplateBatchFiles/BUSTED.bf )" | qsub -l walltime=48:00:00 ;
 done
--- runSets.sh
+++ runSets.sh
@@ -3,4 +3,5 @@ for k in *.csv.sim.*; do
   simname=${PWD}/$k;
   faname=$simname.fasta;
   (echo $simname; echo $faname) | /usr/local/bin/HYPHYMP ${PWD}/../f3dsims.bf;
+  #echo "((echo $simname; echo $faname) | /usr/local/bin/HYPHYMP ${PWD}/../f3dsims.bf )" | qsub -l walltime=48:00:00 -q eternity ;
 done
